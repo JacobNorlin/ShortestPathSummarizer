04adade2f40637fd390d4595b9efbbcfad6d8713
JacobNA <jacob.norlinandersson@gmail.com>
2015-12-17 12:18:04 +0100

    Fixed word count being wrong

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,16 +9,11 @@ import Types.Types._
 import scala.reflect.io.File
 
 object Main extends App{
-val testText = "Dottern Susanna gifte sig 1607 med John Hall, en berömd läkare. Shakespeare gjorde paret till sina huvudarvingar, över hustrun och den andra dottern. Dottern Judith gifte sig med Thomas Quiney 10 februari 1616. De gifte sig i Holy Trinity Church. Pastorsadjunkten Richard Watts som senare vigde även Quigleys syster Mary var troligen officierande präst[27] Bröllopet ägde rum under fastan vilket var förbjudet. År 1616 hade fastan inletts den 23 januari och den avslutades den 7 april efter påsk. Detta innebar att äktenskapet krävde en särskild licens som kunde utfärdas av biskopen av Worcester, detta hade dock paret inte låtit göra. Walter Wright från Stratford dömdes för att ha vigts utan varken lysning eller licens så eftersom Quiney endast dömdes för att ha vigts utan licens har slutsatsen dragits att Quiney lät lysa i kyrkan[27] Detta regelbrott var av mindre betydelsefull art som förefaller ha orsakats av prästen då tre andra par också vigdes under februari. Quiney kallades dock ändå att infinna sig inför rätta i Worcester. Quiney underlät att infinna sig i rätten, och dömdes till exkommunikation. Det är inte känt huruvida även Judith blev bannlyst. Straffet för Quiney blev inte långvarigt, i november samma år visar källorna att paret var upptagna i kyrkan igen, och tilläts döpa sitt första barn.\n\nÄktenskapet började inte bra, Quiney hade kort dessförinnan gjort en annan kvinna, Margaret Wheeler, med barn. Wheeler dog när hon födde barnet som även det avled, och båda begrovs den 15 mars 1616. Den 26 mars 1616 ställdes Quiney inför rätta och han erkände då att han haft sex utom äktenskapet med Wheeler, och han förklarade sig villig att ta sitt straff. Han dömdes till ett skamstraff som skulle avtjänas inför församlingen tre söndagar i rad, och han tvingades också erkänna sin skuld inför prästen i hans hemförsamling[30] Merparten av straffet efterskänktes dock och hans slutliga straff kom att bestå av böter på fem shilling som gick till de fattiga.\n\nDetta bidrog till utformningen av det testamente, som undertecknades 25 mars samma år."
-//
-//  val summary = createGraph(tokenizedStemmedText)
-  //    for(i <- summary){
-  //        println(tokenizedText(i))
-  //    }
+
 
   val sum = new Summarizer()
 
-  println(sum.summarizeDocument("test.txt",  500))
+  println(sum.summarizeDocument("test.txt",  200))
 
   class Summarizer(){
 
@@ -26,6 +21,10 @@ val testText = "Dottern Susanna gifte sig 1607 med John Hall, en berömd läkare
       val text = scala.io.Source.fromFile(path)("UTF-8").getLines.reduceLeft(_+_)
       //println(text)
       summarizeText(text, targetWordCount)
+    }
+
+    def countWords(text: String) = {
+      text.split(' ').length
     }
 
 
@@ -36,8 +35,9 @@ val testText = "Dottern Susanna gifte sig 1607 med John Hall, en berömd läkare
     }
 
     def determineBestSummary(summaries: List[String], targetWordCount: Int) = {
+      println(summaries.map(countWords) mkString "\n")
       summaries.minBy(summary => {
-        Math.abs(targetWordCount - summary.length)
+        Math.abs(targetWordCount - countWords(summary))
       })
 
     }
